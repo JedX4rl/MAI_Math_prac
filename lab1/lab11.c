69498bb6f6d99c4af7f447eab224c62322484747
JedX4rl <62381238+JedX4rl@users.noreply.github.com>
2023-09-24 13:58:33 +0300

    Update lab11.c

--- lab1/lab11.c
+++ lab1/lab11.c
@@ -3,8 +3,9 @@
 #include <math.h>
 #include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
 
-void Info()
+void info()
 {
     printf("\n");
     printf("Tretiakov Nikita Mikhailovich M8O-213B-22\n");
@@ -12,14 +13,14 @@ void Info()
     printf("\n");
 }
 
-void Error()
+void error()
 {
     printf("Wrong format\n");
     printf("Enter:\"filename\" \"long long int number\" \"flag starts with '-' or '/'\"\n");
     printf("\n");
 }
 
-void Usage()
+void usage()
 {
     printf("-h to print natural numbers are multiples of this and less than or equal to 100\nor to say if there are no suitable\n\n");
     printf("-p to find out if the number is simple or not simple\n\n");
@@ -30,26 +31,28 @@ void Usage()
 
 }
 
-bool Converter(char s[], long long int *res)
+bool converter(char s[], long long int *res)
 {
-    int lenght = strlen(s);
-    for(int i = 0; i < lenght; i++)
-    {
-        if(s[i] < 48 || s[i] > 57)
+    int length = strlen(s);
+    int flag = 0;
+    if (s[0] != '-')
+    {   for(int i = 1; i < length; i++)
         {
-            printf("Your number must be in \"int\" type\n");
-            return true;
+            if(!(isdigit(s[i])))
+            {
+                flag = 1;
+                return true;
+            }
         }
     }
 
-    if (s[0] == '-')
+    if (s[0] == '-' && flag == 0)
     {
-        if (lenght > 20)
+        if (length >= 20)
             {
-                printf("You are out of range");
                 return true;
             }
-        for (int i = 1; i < lenght; i++)
+        for (int i = 1; i < length; i++)
         {
             *res = *res * 10 + (s[i] - '0');
         }
@@ -57,13 +60,12 @@ bool Converter(char s[], long long int *res)
         return false;  
     }
 
-    if (lenght > 19)
+    if (length >= 19)
     {
-        printf("You are out of range");
         return true;
     }
 
-    for (int i = 0; i < lenght; i++)
+    for (int i = 0; i < length; i++)
     {
         *res = *res * 10 + (s[i] - '0');
     }
@@ -71,33 +73,50 @@ bool Converter(char s[], long long int *res)
     
 }
 
-bool Is_simple(long long int n)
+
+int is_simple(long long int n)
 {
-    for (long long i = 2; i < n/2; i++)
+    if (n <= 1)
+        return -1;
+
+    if(n != 2 && n%2 == 0)
+    {
+        return -2;
+    }
+
+
+    for (long long i = 3; i < sqrt(n); i+=2)
     {
         if (!(n % i))
             {
-                return true;
+                return -2;
             }
     }
-    return false;
+    return 0;
 }
 
-void Number_to_string(long long int n)
+char* number_to_string(long long int n)
 {
-    int lenght = 1;
+    int length = 1;
     long long int temp = n;
     char* string;
 
     while (temp /= 10)
-        ++lenght;
+        ++length;
 
     temp = llabs(n);
 
 
     if (n > 0)
-    {   int size = lenght * 2;
+    {   int size = length * 2;
         string = (char*)malloc(sizeof(char) * size);
+
+        if(string == NULL)
+        {
+            free(string);
+            return false;
+        }
+
         string[size-1] = '\0';
         for (int i = size - 2; i >= 0; i-=2)
         {
@@ -109,8 +128,15 @@ void Number_to_string(long long int n)
     }
 
     if (n < 0)
-    {   int size = lenght * 2 + 1;
+    {   int size = length * 2 + 1;
         string = (char*)malloc(sizeof(char) * size);
+        
+        if(string == NULL)
+        {
+            free(string);
+            return false;
+        }
+
         string[size-1] = '\0';
         for (int i = size - 2; i >= 0; i-=2)
         {
@@ -121,13 +147,33 @@ void Number_to_string(long long int n)
         }
         string[0] = '-';
     }
-    printf("%s", string);
-    free(string);
-    //return string;
+    return string;
 
 }
 
-int Summary(long long int n)
+int table_of_powers(long long int n , long long table[][10])
+{
+    
+    for(int i = 1; i <= 10; i++)
+    {
+    long long int result = i;
+    table[0][i-1] = result;
+
+        for (int j = 2; j <= n; j++)
+        {
+            result *= i;
+            table[j-1][i-1] = result;
+
+        }
+        
+    }
+    return 0;
+    
+    
+
+}
+
+int sum(long long int n)
 {
     if (n < 1)
         return 0;
@@ -135,34 +181,36 @@ int Summary(long long int n)
     return ((n-1)/2*n);
 }
 
-int Factorial(long long int n)
+int factorial(long long int n)
 {
     if (n == 0)
         return 1;
-    return n*Factorial(n-1);
+    return n*factorial(n-1);
 }
-
 
 
 
 int main(int argc, char* argv[])
 {
-    Info();
+    info();
 
     if(argc != 3 || (*argv[2] != '-' && *argv[2] != '/') || strlen(argv[2]) != 2)
     {
-        Error();
-        Usage();
+        error();
+        usage();
         return 1;
     }
 
     long long int temp = 0;
 
-    if (Converter(argv[1], &temp))
+    if (converter(argv[1], &temp))
     {
+        printf("Your number must be in \"long long int\" type\n");
         return 2;
-    }
-
+    }  
+   
+   
+   
     if (argv[2][1] == 'h')
     {
         if (temp <= 0 || temp > 100)
@@ -188,10 +236,10 @@ int main(int argc, char* argv[])
 
     if (argv[2][1] == 'p')
     {
-        if(temp <= 0)
-            printf("The number must be greater than 0");
+        if(is_simple(temp) == -1)
+            printf("The number must be greater than 1");
 
-        else if (Is_simple(temp))
+        else if (is_simple(temp) == -2)
             printf("The number %d is NOT simple!", temp);
         else
             printf("The number %d is simple!", temp);
@@ -199,45 +247,63 @@ int main(int argc, char* argv[])
 
     if (argv[2][1] == 's')
     {
-        //printf("%s", Number_to_string(temp));
-        Number_to_string(temp);
+        char* s = number_to_string(temp);
+        printf("%s", s);
+        free(s);
     }
 
     if(argv[2][1] == 'e')
     {
-        if (1 > temp || temp > 10)
-            printf("The number must be greater than zero and less then eleven");
-        else
+        if(1 > temp || temp > 10)
+
         {
-            for (int i = 1; i <= 10; i++)
+            printf("The number must be greater than zero and less then eleven");
+            return -5;
+        
+        }
+
+        long long table[temp][10];
+
+        table_of_powers(temp, table);
+
+        for (int i = 0; i < temp; i++)
+
             {
-                for (long long j = 1; j <= temp; j++)
-                {
-                    printf("%.0f\t", pow(i,j));
-                }
+                printf("|%5d|", i+1);
+                
+                for (int j = 0; j < 10; j++)
+
+                    printf("%-12lld", table[i][j]);
+
                 printf("\n");
             }
-        }
     }
 
     if(argv[2][1] == 'a')
     {
-        printf("%d", Summary(temp));
+        printf("%d", sum(temp));
     }
 
     if(argv[2][1] == 'f')
     {
-        if(temp > 20)
-            {
-                printf("Overflow");
-                return 3;
-            }
-        printf("%d", Factorial(temp));
+        if(temp >= 20)
+        {
+            printf("Overflow");
+            return 3;
+        }
+        
+        if(temp < 0)
+        {
+            printf("Number must be greater than or equal to 0");
+            return 7;
+        }
+
+        printf("%d", factorial(temp));
     }
 
     if(argv[2][1] != 'h' && argv[2][1] != 'p' && argv[2][1] != 's' && argv[2][1] != 'e' && argv[2][1] != 'a' && argv[2][1] != 'f')
     {
-        Usage();
+        usage();
         return 4;
     }
 
